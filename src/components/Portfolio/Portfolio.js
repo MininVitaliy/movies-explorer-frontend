e359eb8fe6a1f2b711d9cb9327e2c777498bc97d
Minin Vitaliy <mini.vitali@yandex.ru>
2023-02-11 17:30:46 +0300

    fix: исправить по замечаниям ревью и загрузить из ветки level-2 работу Diploma 5 коммит JSX доп замечания ревью по Lang и ссылкам на свои проекты

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,23 +4,23 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h4 className="portfolio__title">Портфолио</h4>
-      <ul className="portfolio__list">
-        <li className="portfolio__cell">
+      <nav className="portfolio__list">
+        <a href="https://github.com/MininVitaliy/how-to-learn" target="_blank"
+           className="portfolio__cell header__transition">
           <p className="portfolio__info">Статичный сайт</p>
-          <a href="https://github.com/MininVitaliy/how-to-learn" target="_blank"
-                className="portfolio__icon header__transition">↗</a>
-        </li>
-        <li className="portfolio__cell">
+          <p className="portfolio__icon">↗</p>
+        </a>
+        <a href="https://github.com/MininVitaliy/russian-travel" target="_blank"
+           className="portfolio__cell header__transition">
           <p className="portfolio__info">Адаптивный сайт</p>
-          <a href="https://github.com/MininVitaliy/russian-travel" target="_blank"
-                className="portfolio__icon header__transition">↗</a>
-        </li>
-        <li className="portfolio__cell">
+          <p className="portfolio__icon">↗</p>
+        </a>
+        <a href="https://github.com/MininVitaliy/express-mesto-gha" target="_blank"
+           className="portfolio__cell header__transition">
           <p className="portfolio__info">Одностраничное приложение</p>
-          <a href="https://github.com/MininVitaliy/express-mesto-gha" target="_blank"
-                className="portfolio__icon header__transition">↗</a>
-        </li>
-      </ul>
+          <p className="portfolio__icon">↗</p>
+        </a>
+      </nav>
     </section>
   )
 }
